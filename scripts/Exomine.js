3018a82595a137e7a31957358a462482254dd11c
Gracie Parce <graceehparce@gmail.com>
2022-08-11 11:55:57 -0500

    skeleton

---
+++ scripts/Exomine.js
@@ -0,0 +1,37 @@
+export const Exomine = () => {
+    return `
+        <header class="header">
+            <h1 class="title"></h1>
+        </header>
+        <article class="topBox">
+            <section class="choicesBox">
+                <div>
+                    <h2>Choose Governor</h2>
+                    ${()}
+                </div>
+                <div>
+                    <h2>Choose Facility</h2>
+                    ${()}
+                </div>
+            </section>
+            <section class="colonyInventoryBox">
+                <h2>Colony Mineral Inventory</h2>
+                ${()}
+            </section>
+        </article>
+        <article class="bottomBox">
+            <section class="facilityMineralBox">
+                <h2>Facility Minerals for Sale</h2>
+                ${()}
+            </section>
+            <section class="cartBox">
+                <h2>Space Cart</h2>
+                ${()}
+                <button id="purchase">Purchase Mineral</button>
+            </section>
+        </article>
+
+    
+
+    `
+}
