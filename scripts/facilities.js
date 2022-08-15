d18367a3c04d0f149712eb9cc1cfd949d41f3347
Matt <mattmartino54@gmail.com>
2022-08-15 10:23:23 -0500

    Added check for active statements, and set some governors to inactive.

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -9,12 +9,11 @@ export const Facilities = () => {
     html += `<label>Which Facility?</label>`
     html += `<select name="facility">`
     for (let facility of facilities) {
+        if (facility.active === true){
         if (transientState.selectedFacility === facility.id) {
             html += `<option name="facility" selected value="${facility.id}">${facility.name}</option>`
         } else { html += `<option name="facility"  value="${facility.id}">${facility.name}</option>` }
-
-
-    }
+    }}
     html += `</select>`
     html += `</fieldset>`
     return html
